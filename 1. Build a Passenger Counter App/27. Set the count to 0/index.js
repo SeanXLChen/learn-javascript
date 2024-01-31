09d8b0ac524295adf125c9d1584579db2ab43268
SeanXLChen <xiaolaiseanchen@gmail.com>
2024-01-30 22:00:27 -0800

    reset to zero when save

--- 1. Build a Passenger Counter App/27. Set the count to 0/index.js
+++ 1. Build a Passenger Counter App/27. Set the count to 0/index.js
@@ -10,5 +10,7 @@ function increment() {
 function save() {
     let countStr = count + " - "
     saveEl.textContent += countStr
+    count = 0;    // reset count each time we save
+    countEl.textContent = count;
 }
 
